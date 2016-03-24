f3ee366b745c08302765f42c3bc96141baa52e9f
gchungsy <gchungsy@gmail.com>
2016-03-24 03:10:54 +0000

    Remembering the user

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160315195449) do
+ActiveRecord::Schema.define(version: 20160324025422) do
 
   create_table "posts", force: :cascade do |t|
     t.text     "description"
@@ -29,6 +29,7 @@ ActiveRecord::Schema.define(version: 20160315195449) do
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
     t.string   "password_digest"
+    t.string   "remember_digest"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
